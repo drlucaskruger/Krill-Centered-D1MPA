d71b4272d43268d3bc25319c08dfeaf08d5ff3ed
Lucas Krüger <88837519+drlucaskruger@users.noreply.github.com>
2024-05-07 12:18:00 -0300

    Update 03_coverage.R

--- 03_coverage.R
+++ 03_coverage.R
@@ -1,8 +1,16 @@
-rm(list=c())
+
+
+
+rm(list=ls())
+
 gc()
 
+
+library(lubridate)
 library(raster)
 library(dplyr)
+library(sp)
+library(trip)
 library(ggplot2)
 library(patchwork)
 library(terra)
@@ -82,7 +90,7 @@ tif_directory3 <- "C:/D1MPA trimestral/D1MPA Fishable Area/Layers 2018/geotiff"
 # List all TIF files in the directory
 tif_files3 <- list.files(tif_directory3, pattern = ".tif$", full.names = T)
 
-
+tif_files3
 # Load all TIF files into a raster stack
 raster_stack3 <- terra::rast(tif_files3)
 plot(raster_stack3)
@@ -154,7 +162,7 @@ ext.st<-terra::extract(rasters, stvect)
 
 head(ext.st)
 
-st.df <- data.frame(FA=stvect$Fishable,SA=stvect$SubArea,ext.st[2:71])
+st.df <- data.frame(FA=stvect$Fishable,SA=stvect$SubArea,ext.st[2:61])
 
 head(st.df)
 
@@ -184,7 +192,7 @@ names(gpzvect)[names(gpzvect) == "SubAreas"] <- "SA"
 
 ext.gpz<-terra::extract(rasters, gpzvect)
 
-gpz.df <- data.frame(FA=sfdf$Fishable,SA=gpzvect$SA,ext.gpz[2:71])
+gpz.df <- data.frame(FA=sfdf$Fishable,SA=gpzvect$SA,ext.gpz[2:61])
 
 #gpz.df$SA[is.na(gpz.df$SA)]<-"Outside"
 
@@ -194,18 +202,18 @@ summary(as.factor(gpz.df$SA))
 
 ###-----coverage calcualtion ---------
 
-total<- data.frame(st.df[3:72]) %>%
+total<- data.frame(st.df[3:62]) %>%
   #group_by(FA) %>%
   summarise_all(sum, na.rm = TRUE)
 
-totalF<- data.frame(st.df[1],st.df[3:72]) %>%
+totalF<- data.frame(st.df[1],st.df[3:62]) %>%
   group_by(FA) %>%
   summarise_all(sum, na.rm = TRUE)
 
 totalF<-subset(totalF,FA=="Yes")
 
 
-SAO <- st.df[2:72] %>%
+SAO <- st.df[2:62] %>%
   group_by(SA) %>%
   summarise_all(sum, na.rm = TRUE)
 
@@ -217,7 +225,7 @@ SAF<-subset(SAF,FA=="Yes")
 SAO
 SAF
 
-GPZ <- gpz.df[2:72] %>%
+GPZ <- gpz.df[2:62] %>%
   group_by(SA) %>%
   summarise_all(sum, na.rm = TRUE)
 
@@ -267,12 +275,6 @@ alldf$sa.fa.prop<-alldf$SA.coverF/alldf$SA.cover
 
 alldf$sa.fa.prop[is.na(alldf$sa.fa.prop)]<-0
 
-ggplot(alldf,aes(layers,sa.fa.prop))+
-  geom_segment( aes(x=layers, xend=layers, y=0.75, yend=sa.fa.prop),
-                linetype="dotted") +
-  geom_hline(yintercept = 0.75)+
-  geom_point()+theme_bw()+ylab("Proportion of layers higher importance within fishable area")+
-  coord_flip()+facet_wrap(SA~.)
 
 ### if a layer has more than 75% of its occurrence within fishable area, it is a priority
 
@@ -303,11 +305,14 @@ alldf$types[alldf$type=="X"]<-"Krill updated"
 alldf$types[alldf$type=="O"]<-"layers 2018"
 alldf$types[is.na(alldf$types)]<-"predators updated"
 
+alldf$types[alldf$layers=="obj_5b_Esuperba"]<-"Krill updated"
+
 summary(as.factor(alldf$types))
 
 layers_2018<-subset(alldf,types=="layers 2018")
 predators<-subset(alldf,types=="predators updated")
-krill<-subset(alldf,types=="Krill updated")
+
+krill<-subset(alldf,types=="Krill updated" )
 
 
 gpzS<-subset(predators,layers!="Penguin_Gentoo_AMJ" & layers!="Penguin_Gentoo_JAS" &
@@ -316,7 +321,8 @@ gpzS<-subset(predators,layers!="Penguin_Gentoo_AMJ" & layers!="Penguin_Gentoo_JA
                layers!="Seal_Elephant_AMJ"&layers!="Seal_Elephant_JAS"&
                layers!="Seal_Fur_AMJ"&layers!="Seal_Fur_JAS"&
                layers!="Whale_Humpback_AMJ"&layers!="Whale_AntMinke_AMJ"&
-               layers!="Penguin_Emperor_Cols" )
+               layers!="Penguin_Emperor_Cols" & layers!="obj_5b_Esuperba" & 
+               layers!="AFS_BS_SOI" )
 #winter layers
 
 
@@ -344,7 +350,7 @@ gpzW<-subset(predators,layers=="Penguin_Gentoo_AMJ" | layers=="Penguin_Gentoo_JA
 
 ### layers 2024 
 
-(ggplot(krill,aes(layers,GPZ.prop,colour=priority))+
+(ggplot((krill),aes(layers,GPZ.prop,colour=priority))+
     geom_segment( aes(x=layers, xend=layers, y=0.2, yend=GPZ.prop),
                   linetype="dotted") +
     geom_point(size=2)+
@@ -357,7 +363,7 @@ gpzW<-subset(predators,layers=="Penguin_Gentoo_AMJ" | layers=="Penguin_Gentoo_JA
 
 # summer layers
 
-(ggplot(gpzS,aes(layers,GPZ.prop,colour=priority))+
+(ggplot((gpzS),aes(layers,GPZ.prop,colour=priority))+
     geom_segment( aes(x=layers, xend=layers, y=0.5, yend=GPZ.prop),
                   linetype="dotted") +
     geom_point(size=2)+
@@ -366,7 +372,7 @@ gpzW<-subset(predators,layers=="Penguin_Gentoo_AMJ" | layers=="Penguin_Gentoo_JA
     scale_colour_manual(values=c("blue","red"))+
     ggtitle(label="")+theme_bw()+
     xlab("Summer layers")+ylab("Proportion of GPZ coverage")+
-   ggtitle(label="b. Layers 2024 Summer"))/
+   ggtitle(label="b. Layers 2024 October to March"))/
 
 # a part of fish habitat is inside SOISS MPA. That might increase coverage
 
@@ -374,7 +380,7 @@ gpzW<-subset(predators,layers=="Penguin_Gentoo_AMJ" | layers=="Penguin_Gentoo_JA
 
 #Winter layers
 
-(ggplot(gpzW,aes(layers,GPZ.prop,colour=priority))+
+(ggplot((gpzW),aes(layers,GPZ.prop,colour=priority))+
     geom_segment( aes(x=layers, xend=layers, y=0.5, yend=GPZ.prop),
                   linetype="dotted") +
     geom_point(size=2)+
@@ -383,8 +389,278 @@ gpzW<-subset(predators,layers=="Penguin_Gentoo_AMJ" | layers=="Penguin_Gentoo_JA
     scale_colour_manual(values=c("blue","red"))+
     ggtitle(label="")+theme_bw()+
     xlab("Winter layers")+ylab("Proportion of GPZ coverage")+
-    ggtitle(label="c. Layers 2024 Winter"))
+    ggtitle(label="Layers 2024 April to September"))
 
-# given the dependency of Elephant Seals to Krill, we will not increase coverage in winter 
-# one small area in Bransfield (king george island, outside admiralty bay) is important for gentoos in breeding and in winter 
-# so converted to year-round 
+
+
+
+### now, do the same with the SPZ considered
+
+
+
+### load subareas and strata files
+
+strata<-shapefile("C:/D1MPA trimestral/D1MPA Fishable Area/Shapefiles/Subareas_Strata.shp")
+plot(strata)
+
+summary(as.factor(strata$SubArea))
+
+gpz<-shapefile("C:/D1MPA trimestral/D1MPA Fishable Area/Shapefiles/GPZ_SPZ_cut.shp")
+head(gpz)
+plot(gpz)
+summary(as.factor(gpz$SubArea))
+
+# preferred fishable area
+
+fa<-shapefile("C:/D1MPA trimestral/D1MPA Fishable Area/Shapefiles/Fishable_Area.shp")
+plot(fa)
+
+### spatial points
+
+spt<-shapefile("C:/D1MPA trimestral/D1MPA Fishable Area/Shapefiles/Grid_Points_D1.shp")
+
+head(spt)
+
+stfa<- (over(x = spt, y = fa))
+
+summary(as.factor(stfa$Fishable))
+
+stfa<-SpatialPointsDataFrame(coordinates(spt),stfa[4],proj4string = crs(spt))
+
+#-----strata coverage -----------
+
+st.points<- (over(x = stfa, y = strata))
+
+summary(as.factor(st.points$SubArea))
+
+stratas<-data.frame(st.points[4],stfa[1], coordinates(spt))
+
+stgrid<-SpatialPointsDataFrame(coordinates(spt),stratas)
+
+#plot(stgrid)
+
+stvect<-vect(stgrid)
+
+head(stvect)
+
+summary(as.factor(stvect$SubArea))
+
+ext.st<-terra::extract(rasters, stvect)
+
+head(ext.st)
+
+st.df <- data.frame(FA=stvect$Fishable,SA=stvect$SubArea,ext.st[2:61])
+
+head(st.df)
+
+summary(as.factor(st.df$SA))
+summary(as.factor(st.df$FA))
+
+
+###--------- gpz covergae-----
+
+gpz.points<- (over(x = stfa, y = gpz))
+
+gpzs<-data.frame(gpz.points)
+head(gpzs)
+
+summary(as.factor(gpzs$SubArea))
+head(stfa)
+sfdf<-data.frame(stfa,gpzs)
+
+head(sfdf)
+summary(as.factor(sfdf$SubArea))
+
+gpzgrid<-SpatialPointsDataFrame(coordinates(stfa),sfdf)
+
+gpzvect<-vect(gpzgrid)
+
+names(gpzvect)[names(gpzvect) == "SubArea"] <- "SA"
+
+summary(as.factor(gpzvect$SA))
+
+ext.gpz<-terra::extract(rasters, gpzvect)
+
+gpz.df <- data.frame(FA=sfdf$Fishable,SA=gpzvect$SA,ext.gpz[2:61])
+
+#gpz.df$SA[is.na(gpz.df$SA)]<-"Outside"
+
+summary(as.factor(gpz.df$FA))
+summary(as.factor(gpz.df$SA))
+
+
+###-----coverage calcualtion ---------
+
+total<- data.frame(st.df[3:62]) %>%
+  #group_by(FA) %>%
+  summarise_all(sum, na.rm = TRUE)
+
+totalF<- data.frame(st.df[1],st.df[3:62]) %>%
+  group_by(FA) %>%
+  summarise_all(sum, na.rm = TRUE)
+
+totalF<-subset(totalF,FA=="Yes")
+
+
+SAO <- st.df[2:62] %>%
+  group_by(SA) %>%
+  summarise_all(sum, na.rm = TRUE)
+
+SAF <- st.df %>%
+  group_by(FA,SA) %>%
+  summarise_all(sum, na.rm = TRUE)
+
+SAF<-subset(SAF,FA=="Yes")
+SAO
+SAF
+
+GPZ <- gpz.df[2:62] %>%
+  group_by(SA) %>%
+  summarise_all(sum, na.rm = TRUE)
+
+GPF <- gpz.df %>%
+  group_by(FA,SA) %>%
+  summarise_all(sum, na.rm = TRUE)
+
+GPF<-subset(GPF,FA=="Yes")
+
+
+GPZ
+GPF
+
+totm<-melt(total)
+tofm<-melt(totalF)
+
+saom<-melt(SAO,id.vars=c("SA"))
+safm<-melt(SAF,id.vars=c("FA","SA"))
+
+gpzm<-na.omit(melt(GPZ,id.vars=c("SA")))
+gpfm<-na.omit(melt(GPF,id.vars=c("FA","SA")))
+summary(as.factor(gpzm$SA))
+summary(as.factor(gpfm$SA))
+
+names(totm)[names(totm) == "value"] <- "range"
+names(saom)[names(saom) == "value"] <- "SA.cover"
+names(gpzm)[names(gpzm) == "value"] <- "GPZ.cover"
+
+names(tofm)[names(tofm) == "value"] <- "rangeF"
+names(safm)[names(safm) == "value"] <- "SA.coverF"
+names(gpfm)[names(gpfm) == "value"] <- "GPZ.coverF"
+
+head(gpfm)
+
+head(safm)
+
+head(tofm)
+
+gpz.st<-merge(gpzm,saom,all=T)
+
+alldf<-merge(gpz.st,totm,all=T)
+head(alldf)
+
+alldff1<-merge(alldf,tofm)
+alldff2<-merge(alldff1,safm)
+alldf<-merge(alldff2,gpfm)
+
+head(alldf)
+
+names(alldf)[names(alldf) == "variable"] <- "layers"
+
+alldf$sa.fa.prop<-alldf$SA.coverF/alldf$SA.cover
+
+alldf$sa.fa.prop[is.na(alldf$sa.fa.prop)]<-0
+
+
+### if a layer has more than 75% of its occurrence within fishable area, it is a priority
+
+
+alldf$priority<-ifelse(alldf$sa.fa.prop>0.75,"Priority","Non-priority")
+
+alldf$GPZ.prop<-alldf$GPZ.cover/alldf$SA.cover
+
+alldf$GPZ.prop[is.na(alldf$GPZ.prop)]<-0
+
+
+
+head(alldf)
+
+
+#summer layers
+
+
+# specify layers. If it starts with X they are krill for the updated layers
+# if O, they are layers from 2018
+# if other, they are updated predator layers
+
+alldf$type<-substring(alldf$layers,first=1,last=1)
+
+summary(as.factor(alldf$type))
+
+alldf$types[alldf$type=="X"]<-"Krill updated"
+alldf$types[alldf$type=="O"]<-"layers 2018"
+alldf$types[is.na(alldf$types)]<-"predators updated"
+
+alldf$types[alldf$layers=="obj_5b_Esuperba"]<-"Krill updated"
+
+summary(as.factor(alldf$types))
+
+
+
+summary(as.factor(alldf$priority))
+
+prior<-subset(alldf,priority=="Priority")
+
+krillSPZ<-subset(prior,types=="Krill updated" )
+predators<-subset(prior,types=="predators updated")
+
+gpzSPZ<-subset(predators,layers!="Penguin_Gentoo_AMJ" & layers!="Penguin_Gentoo_JAS" &
+               layers!="Penguin_Chinstrap_AMJ" & layers!="Penguin_Chinstrap_JAS"&
+               layers!="Seal_Crabeater_AMJ"&layers!="Seal_Crabeater_JAS"&
+               layers!="Seal_Elephant_AMJ"&layers!="Seal_Elephant_JAS"&
+               layers!="Seal_Fur_AMJ"&layers!="Seal_Fur_JAS"&
+               layers!="Whale_Humpback_AMJ"&layers!="Whale_AntMinke_AMJ"&
+               layers!="Penguin_Emperor_Cols" & layers!="obj_5b_Esuperba" & 
+                 layers!="AFS_BS_SOI" & layers!="Seal_Crabeater_OND" )
+
+gpzS1<-subset(gpzS,priority=="Priority")
+
+krillS1<-subset(krill,priority=="Priority")
+
+krillS1$unit<-c("GPZ")
+krillSPZ$unit<-c("GPZ + SPZ")
+
+gpzS1$unit<-c("GPZ")
+gpzSPZ$unit<-c("GPZ + SPZ")
+
+
+krilldf<-rbind(krillS1,krillSPZ)
+
+
+gpzspz2<-rbind(gpzS1,gpzSPZ,subset(krilldf,layers=="X01_Krill_JR_Weddell"))
+gpzspz2<-subset(gpzspz2,layers!="Seal_Crabeater_OND")
+
+
+### layers 2024 GPZ and GPZ+SPZ
+summary(as.factor(gpzspz2$layers))
+
+gpzspz2$layers<-factor(gpzspz2$layers,levels=c("bathy_150_500","bathy_0_150",
+                                               "Whale_Humpback_JFM",
+                                               "Whale_AntMinke_JFM","Seal_Elephant_JFM",
+                                              "IBA_CHP", "IBA_GEP","IBA_ADP",
+                                               "X01_Krill_JR_Weddell"))
+  
+  (ggplot(subset(gpzspz2,SA=="SA 48.1"),aes(layers,GPZ.prop,colour=unit))+
+     geom_segment( aes(x=layers, xend=layers, y=0.5, yend=GPZ.prop),
+                   linetype="dotted") +
+     geom_point(size=2)+
+     geom_hline(yintercept=c(0.5),linetype="dashed")+
+     facet_wrap(SA~.)+coord_flip()+
+     scale_colour_manual(values=c("blue","red"))+
+     ggtitle(label="")+theme_bw()+
+     xlab("Summer layers")+ylab("Proportion")+
+     ggtitle(label=" layers GPZ + SPZ coverage"))
+  
+  
+
+
+  
